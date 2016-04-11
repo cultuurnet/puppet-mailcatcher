f0b04f12efea2fb0e7eebcdeb34e508d4a9ee4dc
Kristof Willaert <kristof.willaert@cultuurnet.be>
2016-04-11 15:42:12 +0200

    Add configuration

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,5 +2,18 @@
 #
 # This class is called from mailcatcher for service config.
 #
-class mailcatcher::config {
+class mailcatcher::config (
+  $http_ip   = $mailcatcher::http_ip,
+  $http_port = $mailcatcher::http_port,
+  $smtp_ip   = $mailcatcher::smtp_ip,
+  $smtp_port = $mailcatcher::smtp_port
+) {
+
+  file { '/etc/default/mailcatcher':
+    ensure  => 'file',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => template('mailcatcher/default/mailcatcher.erb')
+  }
 }
